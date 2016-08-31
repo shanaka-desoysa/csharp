ce3fa0aae783ff088f62656baaceddabc8bd2111
Shanaka DeSoysa <shanaka.desoysa@entrustdatacard.com>
2016-08-31 13:46:07 -0500

    Rename solution

--- OscarMike.GenerateSelfSignedCertificate/OscarMike.GenerateSelfSignedCertificate/Program.cs
+++ OscarMike.GenerateSelfSignedCertificate/OscarMike.GenerateSelfSignedCertificate/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OscarMike.Common.Cryptography;
 
-namespace GenerateSelfSignedCertificate
+namespace OscarMike.GenerateSelfSignedCertificate
 {
     class Program
     {
